8dcbd944c3adef48f32e62212c8fc82c25308a1a
Sarah Weinstein <sweinstein@pivotal.io>
2020-03-21 19:51:54 -0700

    Add 'Invalid Redirect' error response

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const certKeyWords = ['blank apps manager', 'cert', 'my appsman broke', '502'];
 const cloudfoundryapplicationKeyWords = ['cloudfoundryapplication'];
 const websocketKeyWords = ['426', 'upgrade required', 'websocket', 'streaming logs', 'search bar'];
 const uaaKeyWords = ['reset password', 'login page of apps manager'];
+const redirectKeyWords = ['invalid redirect', 'request for authorization was invalidpack'];
 
 const clippyKeyWords = ['clippy'];
 
@@ -19,10 +20,10 @@ const contains = (message, pattern) => {
   pattern.forEach(p => {
     if (message.toLowerCase().includes(p)) {
       flag = true
-    };
-  })
+    }
+  });
   return flag;
-}
+};
 
 const botText = ({message, userName}) => {
   if (contains(message, autoscalerManageWords)) return "Hi <@" + userName+ ">! It sounds like you might be asking a question about the 'Manage' link for autoscaler instances in Apps Manager being an invalid link. This is caused when you upgrade from an earlier version of PAS to PAS 2.3 or later. The autoscaler service instance may still be associated with a `dashboard_url`, which triggers Apps Manager to render the 'Manage' link, but the link leads to a `404`. \n\n The fix is to delete and recreate the autoscaler service instance. Further details can be found in this KB article: <https://community.pivotal.io/s/article/autoscaler-manage-button-in-apps-manager-shows-404-not-found-error> \n\n If this isn't what you're looking for, one of my humans will be with you when they have a moment.";
@@ -31,9 +32,10 @@ const botText = ({message, userName}) => {
   if (contains(message, cloudfoundryapplicationKeyWords)) return "Hi <@" + userName+ ">! It sounds like you might be looking for information on 404 errors displayed in the browser console on requests to a URL ending in `/cloudfoundryapplication`. Apps Manager makes such a request to an app when you visit the app page in Apps Manager, to determine whether the app has Spring or Steeltoe actuator endpoints, and if the app does not, seeing a 404 error in the console is normal. \n\n If the app does have Spring/Steeltoe actuator endpoints and you see the request to those endpoints failing, this KB article may help you diagnose the issue: <https://community.pivotal.io/s/article/Spring-Boot-Actuator-Tabs-Not-Showing-in-Apps-Manager> \n\n If it doesn't have what you're looking for, one of my humans will be with you when they have a moment.";
   if (contains(message, websocketKeyWords)) return "Hi <@" + userName+ ">! It sounds like you might be looking for a solution to failure of the search or streaming logs functionality in Apps Manager on PAS 2.6 or earlier. If your environment is using an untrusted certificate, you should see a prompt to click through to a window in which you can accept the untrusted certificate. After doing so, close the newly-opened window and click the browser refresh button to reload Apps Manager, and the problem should be solved. \n\nIf you are using a trusted certificate or have accepted the untrusted certificate and are still seeing a problem, and the browser console shows a `426 Upgrade Required error`, this indicates that your environment's network infrastructure does not support the necessary WebSocket connection. This is an issue particularly if your environment is using AWS Elastic Load Balancing (ELB), and one solution is to switch to using AWS Application Load Balancers. This forum post has more information on the ELB WebSocket issue: <https://forums.aws.amazon.com/thread.jspa?threadID=231996> \n\n If it doesn't have what you're looking for, one of my humans will be with you when they have a moment.";
   if (contains(message, uaaKeyWords)) return "Hi <@" + userName+ ">! It sounds like you might be asking a question about the Apps Manager login page or user password flow. That functionality is owned by UAA, and questions can be directed to their channel, #pcf-uaa. \n\n If this isn't what you're looking for, one of my humans will be with you when they have a moment.";
+  if (contains(message, redirectKeyWords)) return "Hi <@" + userName+ ">! It sounds like you might be asking a question about an invalid redirect error when logging in to Apps Manager. The details about setting up redirect URIs in this doc might help: <https://docs.pivotal.io/platform/application-service/2-8/operating/configure-multi-foundation.html> \n\n If this isn't what you're looking for, one of my humans will be with you when they have a moment.";
 
   if (contains(message, clippyKeyWords)) return "Hi <@" + userName+ ">! Hi user! It sounds like you appreciate Microsoft's best feature ever. Here are some communities of Clippy's biggest fans. \n <https://www.facebook.com/MicrosoftClippy> \n <https://steamcommunity.com/groups/ClippyFanclub> \n <https://twitter.com/ClippyFans>";
-}  
+};
 
 app.message( async ({ message, context }) => {
   try {
